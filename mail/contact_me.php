d541e167812b36238acdab9fb67fdd60b5b360b4
Jamie Jarzebowski <jjarze@gmail.com>
2018-03-01 10:08:29 +0000

    Added images to info page

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,7 +4,7 @@ if(empty($_POST['name']) || empty($_POST['phone']))
    
 //    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo "Validation failed -  Missing Name or Phone!";
    return false;
    }
    
@@ -16,11 +16,12 @@ $expense = strip_tags(htmlspecialchars($_POST['expense']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'jjarze@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'ssamuels@addedbenefits.co.za';
 $email_subject = "DebtSorted Contact Form:  $name";
 $email_body = "We have received a lead form the DebtSorted contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nGross Income: $income\n\nGross Expenses: $expense\n\nMessage:\n$message";
 $headers = "From: noreply@debtsorted.com\n";
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
+echo "Mail sent: Name: $name\n Number: $phone";
 return true;         
 ?>
